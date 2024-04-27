daf432ee9464e14d93a0ca4648380ff641fff707
Yatindra nath Pandey <142476080+yatindra2004@users.noreply.github.com>
2024-04-27 13:34:44 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,6 +1,6 @@
 let score = true;
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
 let valueInNUmber = Number(score);
 // console.log(typeof valueInNUmber);
@@ -21,8 +21,45 @@ let booleanisLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log( stringNumber);
+// console.log(typeof stringNumber);
+// console.log( stringNumber);
+
+
+//******************  Operation  ******************* */
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2*2);
+// console.log(2**2); pow
+// console.log(2/2);
+
+
+// let str1 = "yatindra";
+// let str2 = "hello";
+
+// let str3 = str1+str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3;
+ num1 = num2 = num3 = 2+2;
+ let gameCounter  = 100
+ ++gameCounter;
+ console.log(gameCounter);
+
 
 
 
