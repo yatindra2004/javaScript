96aa3f758c020f58521ee184f0d75a1fd1d1747d
Yatindra nath Pandey <142476080+yatindra2004@users.noreply.github.com>
2024-04-27 12:55:51 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId= 1234;
+let accountEmail  = "abc@gmail.com";
+var accountPassward = "12345";
+accountCity = "jaipur";
+
+console.table([accountEmail,accountId,accountPassward,accountCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use Strict"; //treat all JS code an newer version
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3);
+console.log("yatindra");
+
+let name = "yatindra";
+let age = 20;
+let isLoggedIn = false;
+
+// datatypes
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => not assign value
+//symbol => unique
+
+//object
+
+console.log(typeof undefined);
+
