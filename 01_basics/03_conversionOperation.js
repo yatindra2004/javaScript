1c7a455b1e456f59f0fd4df8641a1d424fd6fe56
Yatindra nath Pandey <142476080+yatindra2004@users.noreply.github.com>
2024-04-27 13:17:14 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = true;
+
+console.log(typeof(score));
+
+let valueInNUmber = Number(score);
+// console.log(typeof valueInNUmber);
+// console.log(valueInNUmber); 
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+
+let isLoggedIn = 1;
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false;
+// " " => false;
+// "yatindra" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log( stringNumber);
+
+
+
+
