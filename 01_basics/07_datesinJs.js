30ef5c9f8d9d37ad430624bbb7e59494666d9a5a
Yatindra nath Pandey <142476080+yatindra2004@users.noreply.github.com>
2024-04-28 10:48:42 +0000

    date method

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,10 @@
+let myDate = new Date()
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2024,1,24);
+let myCreatedDate = new Date(2024,1,24,5,3)
+console.log(myCreatedDate.toDateString());
